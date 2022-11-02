7118c250b0ff3dc23eb8c33ca98bff299c3b039d
Lucky <saivarshini.chinnasani@gmail.com>
2022-11-02 17:31:55 -0500

    Updated Slower Drive Commands

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DriveCommands/TeleopCommands/SlowerDriveCommand.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.commands.DriveCommands.TeleopCommands;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+
+public class SlowerDriveCommand extends DefaultDriveCommand {
+    public SlowerDriveCommand(Drivetrain drive, GamepadEx driverGamepad) {
+        super(drive, driverGamepad, false);
+        this.multiplier = 0.35;
+    }
+}
