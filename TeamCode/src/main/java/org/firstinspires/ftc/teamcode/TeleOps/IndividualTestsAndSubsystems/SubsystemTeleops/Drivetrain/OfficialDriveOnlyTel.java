a8aabb5659a6fe40e42640fd703785e1c81807a8
Lucky <saivarshini.chinnasani@gmail.com>
2022-11-03 19:48:53 -0500

    Drive Teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/IndividualTestsAndSubsystems/SubsystemTeleops/Drivetrain/OfficialDriveOnlyTel.java
@@ -0,0 +1,90 @@
+package org.firstinspires.ftc.teamcode.TeleOps.IndividualTestsAndSubsystems.SubsystemTeleops.Drivetrain;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.arcrobotics.ftclib.command.button.Button;
+import com.arcrobotics.ftclib.command.button.GamepadButton;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.hardware.ServoEx;
+import com.arcrobotics.ftclib.hardware.motors.CRServo;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.TeleOps.GamepadTrigger;
+import org.firstinspires.ftc.teamcode.commands.DriveCommands.TeleopCommands.DefaultDriveCommand;
+import org.firstinspires.ftc.teamcode.commands.DriveCommands.TeleopCommands.SlowDriveCommand;
+import org.firstinspires.ftc.teamcode.commands.IntakeAndDropConeCommands.DropConeCommand;
+import org.firstinspires.ftc.teamcode.commands.IntakeAndDropConeCommands.PickConeCommand;
+import org.firstinspires.ftc.teamcode.commands.SlideBackCommands.SlideGroundBackCommand;
+import org.firstinspires.ftc.teamcode.commands.SlideBackCommands.SlideHighBackCommand;
+import org.firstinspires.ftc.teamcode.commands.SlideBackCommands.SlideLowBackCommand;
+import org.firstinspires.ftc.teamcode.commands.SlideBackCommands.SlideMidBackCommand;
+import org.firstinspires.ftc.teamcode.commands.SlideBackCommands.SlideResetBackCommandT;
+import org.firstinspires.ftc.teamcode.commands.SlideFrontCommands.SlideGroundFrontCommand;
+import org.firstinspires.ftc.teamcode.commands.SlideFrontCommands.SlideHighFrontCommand;
+import org.firstinspires.ftc.teamcode.commands.SlideFrontCommands.SlideLowFrontCommand;
+import org.firstinspires.ftc.teamcode.commands.SlideFrontCommands.SlideMidFrontCommand;
+import org.firstinspires.ftc.teamcode.commands.SlideFrontCommands.SlideResetFrontCommandT;
+import org.firstinspires.ftc.teamcode.driveTrain.MatchOpMode;
+import org.firstinspires.ftc.teamcode.driveTrain.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.ClawMotors;
+import org.firstinspires.ftc.teamcode.subsystems.ClawServos;
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.Slide;
+
+@Config
+@TeleOp(name = "OfficialDriveOnlyTeleop")
+public class OfficialDriveOnlyTel extends MatchOpMode {
+
+    private static double startPoseX = 0;
+    private static double startPoseY = 0;
+    private static double startPoseHeading = 0;
+
+    //Motors and Servos
+    private MotorEx leftFront, leftRear, rightRear, rightFront;
+    // Gamepad
+    private GamepadEx driverGamepad, operatorGamepad;
+    // Subsystems
+    private Drivetrain drivetrain;
+
+    @Override
+    public void robotInit() {
+        driverGamepad = new GamepadEx(gamepad1);
+        operatorGamepad = new GamepadEx(gamepad2);
+
+        drivetrain = new Drivetrain(new SampleMecanumDrive(hardwareMap), telemetry, hardwareMap);
+        drivetrain.init();
+        drivetrain.setPoseEstimate(new Pose2d(startPoseX, startPoseY, Math.toRadians(startPoseHeading)));
+        drivetrain.setDefaultCommand(new DefaultDriveCommand(drivetrain, driverGamepad, false));
+
+//        standardTrackingWheelLocalizer = new StandardTrackingWheelLocalizer(leftEncoder, rightEncoder, frontEncoder, hardwareMap);
+    }
+
+    //Buttons
+//    private Button slowModeBumper, robotDriveButton, fieldDriveButton;
+
+
+    @Override
+    public void configureButtons() {
+        //Robot/Centric Drive Button
+        Button robotDriveButton = (new GamepadButton(driverGamepad, GamepadKeys.Button.START))
+                .whenPressed(new DefaultDriveCommand(drivetrain,driverGamepad,  true));
+        Button fieldDriveButton = (new GamepadButton(driverGamepad, GamepadKeys.Button.BACK))
+                .whenPressed(new DefaultDriveCommand(drivetrain,driverGamepad,false));
+
+        //Slowmode - D1
+            Button slowModeBumper = (new GamepadButton(driverGamepad, GamepadKeys.Button.RIGHT_BUMPER))
+                    .whileHeld(new SlowDriveCommand(drivetrain, driverGamepad));
+
+    }
+
+    @Override
+    public void matchLoop() { }
+    @Override
+    public void disabledPeriodic() { }
+    @Override
+    public void matchStart() { }
+    @Override
+    public void robotPeriodic(){}
+}
