7d21a0a5899fe6e3c8f640f63d020daebc6d3727
Lucky <saivarshini.chinnasani@gmail.com>
2023-02-20 15:41:21 -0600

    Changed all slide positions from negative to positive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Slide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Slide.java
@@ -32,24 +32,24 @@ public class  Slide extends SubsystemBase {
     private final double encoderOffset = 0;
     private final double encoderOffset2 = 0;
 
-    public static int RESTING_POS = 5;
-    public static int GROUND_POS = -30;
-    public static int LOW_POS = -315;
-    public static int MID_POS = -735;
-    public static int HIGH_POS = -1240;
+    public static int RESTING_POS = -5;
+    public static int GROUND_POS = 30;
+    public static int LOW_POS = 315;
+    public static int MID_POS = 735;
+    public static int HIGH_POS = 1240;
 
-    public static int AUTO_MID_POS = -898;
-    public static int AUTO_HIGH_POS = -1261;
+    public static int AUTO_MID_POS = 898;
+    public static int AUTO_HIGH_POS = 1261;
 
 
     //Auto Slide Positions
-    public static int CONE_STACK_POS = -265;
+    public static int CONE_STACK_POS = 265;
 
-    public static int CONE_1_POS = -20;
-    public static int CONE_2_POS = -100;
-    public static int CONE_3_POS = -135;
-    public static int CONE_4_POS = -156;
-    public static int CONE_5_POS = -315;
+    public static int CONE_1_POS = 20;
+    public static int CONE_2_POS = 100;
+    public static int CONE_3_POS = 135;
+    public static int CONE_4_POS = 156;
+    public static int CONE_5_POS = 315;
     double output = 0;
 
     public static boolean lowBool = false;
