63bf75a9a72af6ee8ec777be5c6dcfb73981e94d
Lucky <saivarshini.chinnasani@gmail.com>
2023-02-20 15:40:41 -0600

    Added Trajectory Container Things

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trajectorysequence/container/Back.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.trajectorysequence.container;
+
+public class Back extends PathSegment {
+    public volatile double distance;
+    public Back(double distance) {
+        this.distance = distance;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trajectorysequence/container/Forward.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.trajectorysequence.container;
+
+public class Forward extends PathSegment {
+    public volatile double distance;
+    public Forward(double distance) {
+        this.distance = distance;
+    }
+}
