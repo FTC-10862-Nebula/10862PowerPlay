db95ef1ce0376490c28127e038c39ab4f2693ff7
Anti-Shulk <84753002+Anti-Shulk@users.noreply.github.com>
2023-02-20 20:28:37 -0600

    right side auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autons/regional/right/RightRegionalAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autons/regional/right/RightRegionalAuto.java
@@ -1,13 +1,18 @@
 package org.firstinspires.ftc.teamcode.opmode.autons.regional.right;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-import org.firstinspires.ftc.teamcode.commands.Drive.AutoCommands.DisplacementCommand;
-import org.firstinspires.ftc.teamcode.commands.Drive.AutoCommands.MarkerCommand;
 import org.firstinspires.ftc.teamcode.commands.Drive.AutoCommands.TrajectorySequenceContainerFollowCommand;
+import org.firstinspires.ftc.teamcode.commands.auto.autoConeCommands.PickConeCommand;
+import org.firstinspires.ftc.teamcode.commands.auto.autoConeCommands.prePickB.PrePickB4Command;
+import org.firstinspires.ftc.teamcode.commands.auto.autoConeCommands.prePickB.PrePickB5Command;
+import org.firstinspires.ftc.teamcode.commands.intakeAndOutake.DropAutoConeCommand;
+import org.firstinspires.ftc.teamcode.commands.slide.SlideResetUpAutonCommand;
 import org.firstinspires.ftc.teamcode.commands.slide.slideFCommands.SlideHighFCommand;
+import org.firstinspires.ftc.teamcode.commands.slide.slideFCommands.SlideMidFCommand;
 import org.firstinspires.ftc.teamcode.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.subsystems.Claw;
 import org.firstinspires.ftc.teamcode.subsystems.Slide;
@@ -19,6 +24,7 @@ import org.firstinspires.ftc.teamcode.subsystems.drive.MecanumDrive;
 import org.firstinspires.ftc.teamcode.subsystems.misc.TagVision;
 import org.firstinspires.ftc.teamcode.trajectorysequence.container.Back;
 import org.firstinspires.ftc.teamcode.trajectorysequence.container.Forward;
+import org.firstinspires.ftc.teamcode.trajectorysequence.container.LineToLinearHeading;
 import org.firstinspires.ftc.teamcode.trajectorysequence.container.Pose2dContainer;
 import org.firstinspires.ftc.teamcode.trajectorysequence.container.SetReversed;
 import org.firstinspires.ftc.teamcode.trajectorysequence.container.SplineTo;
@@ -36,7 +42,7 @@ public class RightRegionalAuto extends MatchOpMode {
     private TagVision tagVision;
     private TurnServo turnServo;
     @Config
-    public static class LeftRegionalAutoConstants {
+    public static class RightRegionalAutoConstants {
 
         public static class Speed {
             public static double baseVel = DriveConstants.MAX_VEL; // value
@@ -73,27 +79,35 @@ public class RightRegionalAuto extends MatchOpMode {
                 public static SetReversed a = new SetReversed(true);
                 public static Forward b = new Forward(20);
                 public static SplineTo c = new SplineTo(30, -16, -135);
-                static TrajectorySequenceContainer cycle1Pickup = new TrajectorySequenceContainer(Speed::getBaseConstraints, a, b, c);
+                static TrajectorySequenceContainer cycle1Drop = new TrajectorySequenceContainer(Speed::getBaseConstraints, a, b, c);
             }
 
             public static class Cycle2Pickup {
                 public static SetReversed a = new SetReversed(true);
                 public static SplineTo b = new SplineTo(42, -12, 0);
                 public static Back c = new Back(20);
-                static TrajectorySequenceContainer cycle1Pickup = new TrajectorySequenceContainer(Speed::getBaseConstraints, a, b, c);
+                static TrajectorySequenceContainer cycle2Pickup = new TrajectorySequenceContainer(Speed::getBaseConstraints, a, b, c);
             }
 
             public static class Cycle2Drop {
                 public static SetReversed a = new SetReversed(true);
                 public static Forward b = new Forward(20);
                 public static SplineTo c = new SplineTo(30, -16, -135);
-                static TrajectorySequenceContainer cycle1Pickup = new TrajectorySequenceContainer(Speed::getBaseConstraints, a, b, c);
+                static TrajectorySequenceContainer cycle2Drop = new TrajectorySequenceContainer(Speed::getBaseConstraints, a, b, c);
             }
 
             public static class Park {
                 public static double leftX = 12;
                 public static double midX = 24;
                 public static double rightX = 36;
+                public static double y = 12;
+                public static double heading = -90;
+                static TrajectorySequenceContainer getPark(double x) {
+                    return new TrajectorySequenceContainer(
+                            Speed::getBaseConstraints,
+                            new LineToLinearHeading(x, y, heading)
+                    );
+                }
             }
         }
     }
@@ -121,23 +135,61 @@ public class RightRegionalAuto extends MatchOpMode {
         double finalX = 0;
         switch (tagVision.getTag()) {
             case 1:
-                finalX = LeftRegionalAutoConstants.Path.Park.leftX;
+                finalX = RightRegionalAutoConstants.Path.Park.leftX;
                 break;
             case 2:
-                finalX = LeftRegionalAutoConstants.Path.Park.midX;
+                finalX = RightRegionalAutoConstants.Path.Park.midX;
                 break;
             case 3:
-                finalX = LeftRegionalAutoConstants.Path.Park.rightX;
+                finalX = RightRegionalAutoConstants.Path.Park.rightX;
                 break;
         }
         schedule(
                 new SequentialCommandGroup(
-                        run(() -> drivetrain.setPoseEstimate(LeftRegionalAutoConstants.Path.PreLoad.startPose.getPose())),
-                        run(claw::clawClose),
-                        run(slide::slideAutoHigh),
-                        run(arm::moveHighFAuto),
-                        new TrajectorySequenceContainerFollowCommand(drivetrain, LeftRegionalAutoConstants.Path.PreLoad.preload,
-                                new DisplacementCommand(12, run(turnServo::setFClawPos))),
+                        run(() -> drivetrain.setPoseEstimate(RightRegionalAutoConstants.Path.PreLoad.startPose.getPose())),
+
+                        /* Preload */
+                        new ParallelCommandGroup(
+                                new TrajectorySequenceContainerFollowCommand(drivetrain, RightRegionalAutoConstants.Path.PreLoad.preload),
+                                new SlideHighFCommand(slide, arm, claw, turnServo, true)
+                        ),
+                        new DropAutoConeCommand(claw, slide, arm, true),
+
+                        /* Cycle 1 Pickup */
+                        new ParallelCommandGroup(
+                                new PrePickB5Command(slide, claw, arm, turnServo),
+                                new TrajectorySequenceContainerFollowCommand(drivetrain, RightRegionalAutoConstants.Path.Cycle1Pickup.cycle1Pickup)
+                        ),
+                        new PickConeCommand(slide, claw),
+
+                        /* Cycle 1 Drop */
+                        new ParallelCommandGroup(
+                                new SlideMidFCommand(slide, arm, claw, turnServo,true),
+                                new TrajectorySequenceContainerFollowCommand(drivetrain, RightRegionalAutoConstants.Path.Cycle1Drop.cycle1Drop)
+                        ),
+                        new DropAutoConeCommand(claw, slide, arm, true),
+
+                        /* Cycle 2 Pickup */
+                        new ParallelCommandGroup(
+                                new PrePickB4Command(slide, claw, arm, turnServo),
+                                new TrajectorySequenceContainerFollowCommand(drivetrain, RightRegionalAutoConstants.Path.Cycle2Pickup.cycle2Pickup)
+                        ),
+                        new PickConeCommand(slide, claw),
+
+                        /* Cycle 2 Drop */
+                        new ParallelCommandGroup(
+                                new SlideMidFCommand(slide, arm, claw, turnServo,true),
+                                new TrajectorySequenceContainerFollowCommand(drivetrain, RightRegionalAutoConstants.Path.Cycle2Drop.cycle2Drop)
+                        ),
+                        new DropAutoConeCommand(claw, slide, arm, true),
+
+                        /* Park */
+                        new ParallelCommandGroup(
+                                new TrajectorySequenceContainerFollowCommand(drivetrain, RightRegionalAutoConstants.Path.Park.getPark(finalX)),
+                                new SlideResetUpAutonCommand(slide, arm, claw, turnServo)
+                        ),
+
+                        /* Save Pose */
                         run(() -> PoseStorage.currentPose = drivetrain.getPoseEstimate())
                 )
         );
