6ab45d6130144b71c967c58c6a2bb0ee67810355
Lucky <saivarshini.chinnasani@gmail.com>
2023-02-26 18:50:34 -0600

    Changed Wait Times

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/arm/backside/auto/cone/ArmCone2BackCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/arm/backside/auto/cone/ArmCone2BackCommand.java
@@ -13,7 +13,7 @@ import org.firstinspires.ftc.teamcode.subsystems.TurnServo;
 public class ArmCone2BackCommand extends SequentialCommandGroup   {
     public ArmCone2BackCommand(Slide slide, Claw claw, Pivot pivot, TurnServo turnServo){
         addCommands(
-                new WaitCommand(370),
+                new WaitCommand(420),
                 new ParallelCommandGroup(
                         new InstantCommand(claw::clawClose),
 //                        new InstantCommand(claw::clawAutoClose),
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/arm/backside/auto/cone/ArmCone4BackCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/arm/backside/auto/cone/ArmCone4BackCommand.java
@@ -13,7 +13,7 @@ import org.firstinspires.ftc.teamcode.subsystems.TurnServo;
 public class ArmCone4BackCommand extends SequentialCommandGroup   {
     public ArmCone4BackCommand(Slide slide, Claw claw, Pivot pivot, TurnServo turnServo){
         addCommands(
-                new WaitCommand(370),
+                new WaitCommand(420),
                 new ParallelCommandGroup(
                         new InstantCommand(claw::clawClose),
 //                        new InstantCommand(claw::clawAutoClose),
